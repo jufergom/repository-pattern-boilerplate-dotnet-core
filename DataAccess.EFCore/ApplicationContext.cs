800823b761a30cc968279bdc2b2f8c35020e7182
Julio Gomez <jufergom@hotmail.com>
2022-05-17 00:25:48 -0600

    added database context

---
+++ DataAccess.EFCore/ApplicationContext.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.EFCore
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext() { }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options) { }
+
+        /*
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("AppDb");
+            optionsBuilder.UseSq();
+        }
+        */
+
+        public DbSet<Blog> Blogs { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Repository Pattern Boilerplate in .NET Core Web API
-## By jufergom
 
 A .NET Core Web API application containing a Repository Pattern boilerplate code. Sample application is a blog that contains posts. These entities are stored in a sqlite database. The idea is to make this project customizable and modify it to support any SQL database with Entity Framework.
 
@@ -9,3 +8,12 @@ A .NET Core Web API application containing a Repository Pattern boilerplate code
 - https://codewithmukesh.com/blog/entity-framework-core-in-aspnet-core/
 - https://github.com/Unitec-Aplicaciones-Vanguardia/ProductsCatalogApi
 - https://docs.microsoft.com/en-us/ef/core/get-started/overview/first-app?tabs=netcore-cli
+
+## Commands used for database migration
+
+Run the following commands inside the Package Manager Console to execute the sqlite database migration.
+
+```sh
+add-migration Initial
+update-database
+```
