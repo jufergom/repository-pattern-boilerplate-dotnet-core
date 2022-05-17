603caeefc7199120e36d54ac685971b8f4d2f396
Julio Gomez <jufergom@hotmail.com>
2022-05-17 00:28:57 -0600

    deleted commented code

--- DataAccess.EFCore/ApplicationContext.cs
+++ DataAccess.EFCore/ApplicationContext.cs
@@ -15,19 +15,6 @@ namespace DataAccess.EFCore
         public ApplicationContext(DbContextOptions<ApplicationContext> options)
             : base(options) { }
 
-        /*
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
-                .Build();
-
-            var connectionString = configuration.GetConnectionString("AppDb");
-            optionsBuilder.UseSq();
-        }
-        */
-
         public DbSet<Blog> Blogs { get; set; }
 
         public DbSet<Post> Posts { get; set; }
